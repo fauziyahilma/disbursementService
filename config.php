f913841e082a6f37946c9851f5e780507f0cc377
fauziyahilma <48611132+fauziyahilma@users.noreply.github.com>
2019-07-10 12:46:25 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BASEURL', 'http://localhost/disbursement');
+
+define('DB_HOST', 'localhost');
+define('DB_PASS','');
+define('DB_USER', 'root');
+define('DB_NAME', 'disbursement');
+
+?>
